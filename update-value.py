b01fe1853e7de7407ca68ec85caaa7b14876e56b
Lucian Sturião <luciansr@gmail.com>
2019-05-12 00:18:08 -0300

    Remove and add value again
    because only updating was not setting the current value

--- update-value.py
+++ update-value.py
@@ -19,26 +19,28 @@ def updateValueOnEnvironment(apiKey, key, value):
 
                 index = -1
                 i = 0
-
                 action = 'updated'
                 for item in values:
                     if item['key'] == key:
-                        index = i
-                        item['value'] = value
+                        values.pop(i)
                         break
                     i = i + 1
                 
-                if (index == -1):
-                    action = 'added'
+                putResponse1 = requests.put(environmentUrl, data=json.dumps({'environment': fullEnvironment}), headers={"X-API-Key":apiKey})
+
+                if(putResponse1.ok):
+                    if (index == -1):
+                        action = 'added'
+                        
                     values.append({
                         "key": key,
                         "value": value,
                         "enabled": True
                     })
 
-                putResponse = requests.put(environmentUrl, data=json.dumps({'environment': fullEnvironment}), headers={"X-API-Key":apiKey})
+                    putResponse2 = requests.put(environmentUrl, data=json.dumps({'environment': fullEnvironment}), headers={"X-API-Key":apiKey})
 
-                if(putResponse.ok):
-                    print('Updating environment [' + fullEnvironment['name'] + ']: ' + action + ' key [' + key + '] with value [' + value + ']')
+                    if(putResponse2.ok):
+                        print('Updating environment [' + fullEnvironment['name'] + ']: ' + action + ' key [' + key + '] with value [' + value + ']')
 
 updateValueOnEnvironment(sys.argv[1], sys.argv[2], sys.argv[3])
